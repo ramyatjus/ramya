1e88bd5afa91b5c2fbbd83b8f2709f1bb09f2a67
ramyatjus <140580197+ramyatjus@users.noreply.github.com>
2023-08-03 13:27:11 +0530

    Add files via upload

---
+++ dart program/strings.dart
@@ -0,0 +1,16 @@
+void main() {   
+   String str1 = 'this is an example of a single-line string';   
+   String str2 = "this is an example of a double-quotes multiline line string";   
+   String str3 = """this is a multiline line   
+string using the triple-quotes""";   
+  
+   var  a = 10;  
+   var b = 20;  
+   
+   print(str1);  
+   print(str2);   
+   print(str3);   
+  
+ 
+   print("The sum is  = ${a+b}");  
+}  
